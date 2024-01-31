46e2e6023bf2a7e8881d9846894e25436a3aea52
Markus Haas <haas.markus@protonmail.com>
2024-01-31 01:58:24 +0100

    first commit

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -4,20 +4,11 @@ import { NextRequest, NextResponse } from 'next/server';
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body);
-  if (isValid) {
-    try {
-      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://zizzamia.xyz/park-2.png" />
-    <meta property="fc:frame:button:1" content="${accountAddress}" />
-    <meta property="fc:frame:post_url" content="https://zizzamia.xyz/api/frame" />
+    <meta property="fc:frame:button:1" content="https://google.com" />
   </head></html>`);
 }
 
