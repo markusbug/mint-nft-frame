0bbea7fd3651385961c4870aea060d1870cc5127
Markus Haas <haas.markus@protonmail.com>
2024-01-31 12:29:43 +0100

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,12 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
-
+import { FrameRequest, getFrameAccountAddress, getFrameMessage } from '@coinbase/onchainkit';
 
 
 export async function POST(req: NextRequest): Promise<Response> {
-  return Response.redirect(
-    'https://ethosmobile.org',
-    302
-  );
+  try {
+    const body: FrameRequest = await req.json();
+    const { isValid, message } = await getFrameMessage(body);
+    if (isValid) {
+      return new NextResponse(
+      );
+    }
+  } catch(e) {
+    return Response.redirect(
+      'https://ethosmobile.org',
+      302
+    );
+  }
+  
+
+  
+
+  
 }
 
 export const dynamic = 'force-dynamic';
