c7fdf7f95bbcc0306c19f902c33faa4d2c18f84b
Markus Haas <haas.markus@protonmail.com>
2024-01-31 12:07:44 +0100

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,19 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // Set the 302 redirect response to the "/redirect" path
-  const response = new NextResponse(null, {
-    status: 302,
-    headers: {
-      'Location': '/redirect', // Redirect to the "/redirect" path
-    },
-  });
 
-  return response;
-}
 
 export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
+  return Response.redirect(
+    'https://ethosmobile.org',
+    302
+  );
 }
 
 export const dynamic = 'force-dynamic';
